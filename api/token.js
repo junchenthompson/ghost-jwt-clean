7ff179a302a4458a49ef668c1acd5c34ebe37bf1
junchenthompson <junchent@gmail.com>
2025-05-16 15:46:05 -0700

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,14 +1,3 @@
-module.exports = (req, res) => {
-  const payload = {
-    iat: Math.floor(Date.now() / 1000),
-    exp: Math.floor(Date.now() / 1000) + 5 * 60,
-  };
-
-  const token = [
-    'header',
-    Buffer.from(JSON.stringify(payload)).toString('base64url'),
-    'signature',
-  ].join('.');
-
-  res.status(200).json({ token });
-};
+export default function handler(req, res) {
+  res.status(200).json({ success: true });
+}
