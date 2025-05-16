88b56ad5c7760e0ea9c542651854cf0dfabf5349
junchenthompson <junchent@gmail.com>
2025-05-16 16:01:01 -0700

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,20 +1,21 @@
 import jwt from 'jsonwebtoken';
 
 export default function handler(req, res) {
+  const [id, secret] = '68261dae6ab8fa0001a116bb:305710084c895aa9f34bbd49361a7f8c1829033f95acd4e1dcecab766033efa3'.split(':');
+
   const payload = {
     iat: Math.floor(Date.now() / 1000),
-    exp: Math.floor(Date.now() / 1000) + 5 * 60, // 5分钟后过期
+    exp: Math.floor(Date.now() / 1000) + 5 * 60,
+    aud: '/ghost/',
   };
 
-  const secret = 'mysecret'; // 测试用，你可以替换成 Ghost Admin Secret
-
   try {
-    const token = jwt.sign(payload, secret, { algorithm: 'HS256' });
+    const token = jwt.sign(payload, Buffer.from(secret, 'hex'), {
+      algorithm: 'HS256',
+      keyid: id,
+    });
     res.status(200).json({ token });
   } catch (err) {
-    res.status(500).json({
-      error: 'JWT generation failed',
-      details: err.message,
-    });
+    res.status(500).json({ error: 'JWT generation failed', details: err.message });
   }
 }
