bf47cb7f3a7b3e06bd415a81a5caebbce657eac4
junchenthompson <junchent@gmail.com>
2025-05-16 15:57:37 -0700

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,3 +1,17 @@
+import jwt from 'jsonwebtoken';
+
 export default function handler(req, res) {
-  res.status(200).json({ success: true });
+  const payload = {
+    iat: Math.floor(Date.now() / 1000),
+    exp: Math.floor(Date.now() / 1000) + 5 * 60, // 5分钟后过期
+  };
+
+  const secret = 'mysecret'; // 你可以之后用环境变量替代
+
+  try {
+    const token = jwt.sign(payload, secret);
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({ error: 'JWT generation failed', details: err.message });
+  }
 }
