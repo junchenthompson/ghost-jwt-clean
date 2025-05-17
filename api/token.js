fd16f7c7a3712059257e2c3b4bcf0f122970bff0
junchenthompson <junchent@gmail.com>
2025-05-16 19:05:28 -0700

    Update token.js

--- api/token.js
+++ api/token.js
@@ -2,8 +2,8 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res) => {
   const payload = {
-    iat: Math.floor(Date.now() / 1000),                // 当前时间
-    exp: Math.floor(Date.now() / 1000) + 60 * 10,      // 有效期 10 分钟
+    iat: Math.floor(Date.now() / 1000),
+    exp: Math.floor(Date.now() / 1000) + 60 * 10,
     aud: "/v3/admin/",
   };
 
