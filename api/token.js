d762961238167e3ab09887cb2a45b88eeffbbcab
junchenthompson <junchent@gmail.com>
2025-05-18 08:44:25 -0700

    Update token.js

--- api/token.js
+++ api/token.js
@@ -2,17 +2,24 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res) => {
   const [id, secret] = process.env.GHOST_ADMIN_API_KEY.split(':');
+  if (!id || !secret) {
+    return res.status(500).json({ error: "Invalid GHOST_ADMIN_API_KEY format" });
+  }
 
-  const payload = {
-    iat: Math.floor(Date.now() / 1000),
-    exp: Math.floor(Date.now() / 1000) + 10 * 60,
-    aud: "/v3/admin/"
-  };
-
-  const token = jwt.sign(payload, Buffer.from(secret, 'hex'), {
-    keyid: id,
-    algorithm: "HS256"
-  });
+  const token = jwt.sign(
+    {
+      iat: Math.floor(Date.now() / 1000),
+      exp: Math.floor(Date.now() / 1000) + 5 * 60,
+      aud: "/v3/admin/"
+    },
+    Buffer.from(secret, 'hex'),
+    {
+      header: {
+        alg: 'HS256',
+        kid: id
+      }
+    }
+  );
 
   res.status(200).json({ token });
 };
